2429f68b944a14f30c3c54bb375e94fb3854b897
zelinus06 <thai06102001@gmail.com>
2023-06-25 21:36:44 +0700

    'api'

---
+++ hotel-management-api/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using hotel_management_api.Common.Enum;
+
+namespace hotel_management_api.Models
+{
+    public class Booking : BaseModel
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int HotelRoomId { get; set; }
+        public int UserCreateId { get; set; }
+        public DateTime ReservationTime { get; set; }
+        public DateTime CheckinTime { get; set; }
+        public DateTime CheckoutTime { get; set; }
+        public BookingStatusEnum Status { get; set; }
+    }
+}
---
+++ hotel-management-api/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hotel_management_api.Models
+{
+    public class User : BaseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Column(TypeName = "nvarchar(20)")]
+        public string CitizenIdentification { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+        public string NumberPhone { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        [Column(TypeName = "nvarchar(20)")]
+        public string Role { get; set; }
+    }
+}
