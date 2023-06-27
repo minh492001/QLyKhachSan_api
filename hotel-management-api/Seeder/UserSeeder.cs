aa08e8cea78c6c1e692bed7cf4f4044a505edc47
zelinus06 <thai06102001@gmail.com>
2023-06-27 23:33:29 +0700

    'cusService'

---
+++ hotel-management-api/Seeder/UserSeeder.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using hotel_management_api.Models;
+using hotel_management_api.Common;
+
+namespace hotel_management_api.Seeder
+{
+    class UserSeeder
+    {
+        private readonly ModelBuilder _modelBuilder;
+        public UserSeeder(ModelBuilder modelBuilder)
+        {
+            _modelBuilder = modelBuilder;
+        }
+
+        /// <summary>
+        /// Excute data
+        /// </summary>
+        public void SeedData()
+        {
+            _modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    Name = "Nguyễn Văn A",
+                    CitizenIdentification = "001201030041",
+                    DateOfBirth = new DateTime(2001, 01, 01),
+                    NumberPhone = "0123456789",
+                    Address = "Hà Nội",
+                    Email = "nguyenvana@gmail.com",
+                    Username = "nguyenvana@gmail.com",
+                    Password = Untill.CreateMD5("nguyenvana@gmail.com"),
+                    Role = "Manager"
+                },
+                new User
+                {
+                    Id = 2,
+                    Name = "Nguyễn Thị B",
+                    CitizenIdentification = "001201030041",
+                    DateOfBirth = new DateTime(2001, 01, 01),
+                    NumberPhone = "0123456788",
+                    Address = "Hà Nội",
+                    Email = "nguyenthib@gmail.com",
+                    Username = "nguyenthib@gmail.com",
+                    Password = Untill.CreateMD5("nguyenthib@gmail.com"),
+                    Role = "Accountant"
+                },
+                new User
+                {
+                    Id = 3,
+                    Name = "Nguyễn Thị B",
+                    CitizenIdentification = "001201030041",
+                    DateOfBirth = new DateTime(2001, 01, 01),
+                    NumberPhone = "0123456788",
+                    Address = "Hà Nội",
+                    Email = "nguyenthib@gmail.com",
+                    Username = "nguyenthib@gmail.com",
+                    Password = Untill.CreateMD5("nguyenthib@gmail.com"),
+                    Role = "Receptionist"
+                });
+        }
+    }
+}
---
+++ hotel-management-api/Services/CustomerService.cs
@@ -0,0 +1,159 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using hotel_management_api.Common;
+using hotel_management_api.Common.Enum;
+using hotel_management_api.Database;
+using hotel_management_api.Dto;
+using hotel_management_api.Models;
+using hotel_management_api.Repositories;
+using hotel_management_api.Request;
+using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+
+namespace hotel_management_api.Services
+{
+    public class CustomerService
+    {
+        private readonly CustomerRepository _customerRepository;
+        private readonly IMapper _mapper;
+        public CustomerService(ApiOption apiOption, DatabaseContext databaseContext, IMapper mapper)
+        {
+            _customerRepository = new CustomerRepository(apiOption, databaseContext, mapper);
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get user
+        /// </summary>
+        /// <returns></returns>
+        public object GetCustomers(int limit, int page, string? name)
+        {
+            try
+            {
+                var query = this._customerRepository.FindAll();
+                if (name != null)
+                {
+                    query = query.Where(row => row.Name.Contains(name));
+                }
+                query.Skip((page - 1) * limit).Take(limit);
+                var total = query.Count();
+                int tmpByInt = total / limit;
+                double tmpByDouble = (double)total / (double)limit;
+                int totalPage = 1;
+                if(tmpByDouble > (double)tmpByInt)
+                {
+                    totalPage = tmpByInt + 1;
+                }
+                else
+                {
+                    totalPage = tmpByInt;
+                }
+                query = query.Skip((page - 1) * limit).Take(limit);
+                var amount = query.Count();
+                return new
+                {
+                    data = query.ToList(),
+                    Amount = amount,
+                    PageSize = limit,
+                    Total = total,
+                    TotalPage = totalPage,
+                };
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        ///// <summary>
+        ///// Create User
+        ///// </summary>
+        ///// <param name="userStoreRequest"></param>
+        ///// <returns></returns>
+        //public object Store(UserStoreRequest userStoreRequest)
+        //{
+        //    try
+        //    {
+        //        var userCheckList = _customerRepository.FindAll().Where(row => row.Email == userStoreRequest.Email && row.NumberPhone == userStoreRequest.NumberPhone);
+        //        if(userCheckList.Count() > 0)
+        //        {
+        //            throw new Exception("Email or number phone already exist!");
+        //        }
+        //        var user = _mapper.Map<User>(userStoreRequest);
+        //        user.Username = user.Email;
+        //        user.Password = Untill.CreateMD5(user.Email);
+
+        //        _customerRepository.Create(user);
+        //        _customerRepository.SaveChange();
+
+        //        return user;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+
+        ///// <summary>
+        ///// Update user
+        ///// </summary>
+        ///// <param name="id"></param>
+        ///// <param name="userStoreRequest"></param>
+        ///// <returns></returns>
+        //public object Update(int id, UserStoreRequest userStoreRequest)
+        //{
+        //    try
+        //    {
+        //        var user = _userRepository.FindOrFail(id);
+        //        if(user == null)
+        //        {
+        //            throw new Exception("User does not exit!");
+        //        }
+        //        user.Name = userStoreRequest.Name;
+        //        user.Email = userStoreRequest.Email;
+        //        user.Address = userStoreRequest.Address;
+        //        user.DateOfBirth = userStoreRequest.DateOfBirth;
+        //        user.CitizenIdentification = userStoreRequest.CitizenIdentification;
+        //        user.Role = userStoreRequest.Role;
+
+        //        _userRepository.UpdateByEntity(user);
+        //        _userRepository.SaveChange();
+        //        return user;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+
+        ///// <summary>
+        ///// Delete user
+        ///// </summary>
+        ///// <param name="id"></param>
+        ///// <returns></returns>
+        //public object Delete(int id)
+        //{
+        //    try
+        //    {
+        //        var user = _userRepository.FindOrFail(id);
+        //        if (user == null)
+        //        {
+        //            throw new Exception("User does not exit!");
+        //        }
+
+        //        _userRepository.DeleteByEntity(user);
+        //        _userRepository.SaveChange();
+
+        //        return true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+    }
+}
